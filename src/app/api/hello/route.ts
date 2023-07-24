3038fefafc376b54c69d0294cfdb3fdda560ba6f
Marius Obert <obatzer@gmail.com>
2023-07-24 16:54:36 +0200

    chore: prettier

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
-    console.log("Incoming request")
-    const res = new NextResponse("<Response><Message>Hi, great to have you here</Message></Response>");
-    res.headers.set("Content-type", "text/xml")
-    return res;
+  console.log("Incoming request");
+  const res = new NextResponse(
+    "<Response><Message>Hi, great to have you here</Message></Response>",
+  );
+  res.headers.set("Content-type", "text/xml");
+  return res;
 }
