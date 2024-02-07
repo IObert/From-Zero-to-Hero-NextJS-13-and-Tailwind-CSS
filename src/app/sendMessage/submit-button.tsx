6fb960160bf7240c38b91ced57e129d93d4f89b9
Marius Obert <obatzer@gmail.com>
2024-02-07 16:49:06 +0100

    use pnpm and fix deps

--- src/app/sendMessage/submit-button.tsx
+++ src/app/sendMessage/submit-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 
 export function SubmitButton() {
   const { pending } = useFormStatus();
